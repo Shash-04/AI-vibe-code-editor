fa2ae9fa6d93ea0a21267181d4fb604473a76631
Shash-04 <shashwatvaish2@gmail.com>
2026-01-08 12:29:06 +0530

    check 1

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>WE ARE READY FOR NEXT JS PROJECT</div>
+   <div>WE ARE READY FOR NEXT JS PROJECT
+
+    <div>Testing if github is working fine</div>
+   </div>
   );
 }
