7453b0303c89e393a49f22881ae68e26ab375334
Shash-04 <shashwatvaish2@gmail.com>
2026-01-11 19:02:03 +0530

    connected db

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import Image from "next/image";
+import { db } from "@/lib/db";
 
-export default function Home() {
-  return (
-   <div>WE ARE READY FOR NEXT JS PROJECT
-
-    <div>Testing if github is working fine</div>
-   </div>
+export default async function Home() {
+  return ( 
+    <div className=" text-4xl bg-fuchsia-400">BCCL IPO 1K CRORE ISSUE SIZE 21-23</div>
   );
 }
